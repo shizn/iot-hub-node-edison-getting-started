550af47354bbdf596010993940d81195c253ea5c
ZhijunZhao <zhijun.zhao@outlook.com>
2016-10-27 16:42:16 +0800

    Add lesson 1  (#1)
    * Add lesson1
    
    * use gulp-common 0.9.27
    
    * correct typo
    
    * correct typos

---
+++ Lesson1/app/app.js
@@ -0,0 +1,40 @@
+/*
+* IoT Hub Intel Edison NodeJS - Microsoft Sample Code - Copyright (c) 2016 - Licensed MIT
+*/
+'use strict';
+
+var m = require('mraa'); //require mraa
+
+// GPIO pin of the LED
+var CONFIG_PIN = 13;
+// Blink interval in ms
+var INTERVAL = 2000;
+// Total blink times
+var MAX_BLINK_TIMES = 20;
+
+var myLed = new m.Gpio(CONFIG_PIN);
+//set the gpio direction to output
+myLed.dir(m.DIR_OUT);
+
+var blinkTimes = 0;
+/**
+ * Blink LED and log information to console.
+ */
+function blinkLED() {
+  // Terminate process after blinking LED for max allowed times
+  if (blinkTimes >= MAX_BLINK_TIMES) {
+    process.exit(0);
+  }
+
+  blinkTimes++;
+  console.log("[Device] #" + blinkTimes + " Blink LED \n");
+
+  // Light up LED for 100 ms
+  myLed.write(1);
+  setTimeout(function () {
+    myLed.write(0);
+  }, 100);
+}
+
+// Blink LED every other INTERVAL time
+setInterval(blinkLED, INTERVAL);
---
+++ Lesson1/gulpfile.js
@@ -0,0 +1,17 @@
+﻿/*
+* IoT Hub Intel Edison NodeJS - Microsoft Sample Code - Copyright (c) 2016 - Licensed MIT
+*/
+'use strict';
+
+/**
+ * Setup common gulp tasks: init, install-tools, deploy, run
+ */
+require('gulp-common')(require('gulp'), 'edison-node', {
+  appName: 'lesson-1',
+  configTemplate: {
+    "device_host_name_or_ip_address": "[device hostname or IP adress]",
+    "device_user_name": "root",
+    "device_password": "[device password]"
+  },
+  configPostfix: "edison"
+});
